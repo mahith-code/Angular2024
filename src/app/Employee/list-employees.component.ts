dfe54aef8ad9774563acbf9d6b078acd4b08e823
Mahith Rao <mahithrao8833@gmail.com>
2024-08-15 23:10:25 +0530

    Reactive from submitted

---
+++ src/app/Employee/list-employees.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrl: './list-employees.component.css'
+})
+export class ListEmployeesComponent {
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListEmployeesComponent } from './Employee/list-employees.component';
+import { CreateEmployeeComponent } from './Employee/create-employee.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: 'list' , component:ListEmployeesComponent},
+  {path: 'create' , component:CreateEmployeeComponent},
+  {path:'' , redirectTo:'/list', pathMatch:'full'}
+];;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
