dc16816578328f78a0441b978b7e25b8967a80cf
Mahith Rao <mahithrao8833@gmail.com>
2024-09-04 23:04:35 +0530

    Moved validation messages to component from view template

--- src/app/Employee/create-employee.component.ts
+++ src/app/Employee/create-employee.component.ts
@@ -8,36 +8,83 @@ import { FormGroup, FormControl, ReactiveFormsModule, FormBuilder, Validators} f
 })
 export class CreateEmployeeComponent implements OnInit{
   employeeForm!: FormGroup;
-required: any;
+
+
+
+
+validationMessages:any = {
+  'fullName': {
+    'required':'Full name is required',
+    'minlength':'Full name must be greater than 2 characters',
+    'maxlength' : 'Full name must be less than 10 characters'
+  },
+  'email': {
+    'required': 'Email is required'
+  },
+
+    'skillName' :{
+      'required':'Skill Name is required'
+    },
+    'experienceInYears':{
+      'required':'Experience is required',
+    },
+
+    'proficiency': {
+      'required':'Proficiency is required'
+    }
+
+};
+
+formErrors: any = {
+  'fullName' : '',
+  'email' : '',
+  'skillName' : '',
+  'experienceInYears' : '',
+  'proficiency' : '',
+};
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.employeeForm = this.fb.group({
       fullName:['',[Validators.required, Validators.minLength(2) , Validators.maxLength(10)]],
-      email:[''],
+      email:['', Validators.required],
       skills: this.fb.group({
-        skillName :[''],
-        experienceInYears:[''],
-        proficiency:['beginner'],
+        skillName :['', Validators.required],
+        experienceInYears:['', Validators.required],
+        proficiency:['', Validators.required],
       })
+    });
+
+    this.employeeForm.valueChanges.subscribe((data)=> {
+      this.logValidationErrors(this.employeeForm)
     });
   }
 
-  logKeyValuePairs(group: FormGroup):void {
+  logValidationErrors(group: FormGroup = this.employeeForm):void {
     Object.keys(group.controls).forEach((key:string) => {
       const abstractControl = group.get(key);
 
       if(abstractControl instanceof FormGroup) {
-        this.logKeyValuePairs(abstractControl);
-        abstractControl?.disable();
+        this.logValidationErrors(abstractControl);
+
       } else {
-        abstractControl?.markAsDirty();
+        this.formErrors[key] = '';
+        if(abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
+          const messages = this.validationMessages[key];
+
+          for(const errorKey in abstractControl.errors) {
+            if(errorKey) {
+              this.formErrors[key] += messages[errorKey] + '';
+            }
+          };
+        }
       }
     });
   }
   onLoadData():void {
-  this.logKeyValuePairs(this.employeeForm);
+  // this.logValidationErrors(this.employeeForm);
+  // console.log(this.formErrors);
    }
 
   onSubmit(): void {
